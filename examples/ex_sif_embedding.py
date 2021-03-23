80e382c56194cd9791cdf2825caa6e7fbd2b44fb
Macarena Fernandez Urquiza <murquiza@uma-health.com>
2021-03-23 16:45:09 -0300

    [doc] Step by step

--- examples/ex_sif_embedding.py
+++ examples/ex_sif_embedding.py
@@ -12,13 +12,17 @@ rmpc = 1 # number of principal components to remove in SIF weighting scheme
 sentences = ['estudios cardíacos', 'afecciones coronariascd ']
 
 # load word vectors
-(words, We) = data_io.getWordmap(wordfile)
+(words, We) = data_io.getWordmap(wordfile)                      # ({word1:1, word2:2, word3:3, ...}, array de vectores de words)
+
 # load word weights
-word2weight = data_io.getWordWeight(weightfile, weightpara) # word2weight['str'] is the weight for the word 'str'
-weight4ind = data_io.getWeight(words, word2weight) # weight4ind[i] is the weight for the i-th word
+word2weight = data_io.getWordWeight(weightfile, weightpara)     # {word1: freq1, word2: freq2, word3: freq3, ...}
+weight4ind = data_io.getWeight(words, word2weight)              # {1: freq1, 2: freq2, 3: freq3, ...}
+                                                                # si x en word pero no en word2weight -> freqx = 1
+
 # load sentences
-x, m = data_io.sentences2idx(sentences, words) # x is the array of word indices, m is the binary mask indicating whether there is a word in that location
-w = data_io.seq2weight(x, m, weight4ind) # get word weights
+x, m = data_io.sentences2idx(sentences, words)                  # x is the array of word indices
+                                                                # m is the binary mask indicating whether there is a word in that location
+w = data_io.seq2weight(x, m, weight4ind)                        # get word weights
 
 # set parameters
 prms = params.params()
